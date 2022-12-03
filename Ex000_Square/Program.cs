94209bae4287444208d809e4984252f3fe350b74
alekseichernyshev <adanayrel@mail.ru>
2022-12-03 17:43:17 +0900

    Первая итерация задачи 0

---
+++ Ex000_Square/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 0 Вывести квадрат числа. Вернуться после изучения ввода данных.
+
+int number = new Random().Next(2, 1000);
+Console.WriteLine("Вот такое число нарандомили:");
+Console.WriteLine(number);
+Console.WriteLine("Вычисляем...");
+Console.WriteLine(number * number);
