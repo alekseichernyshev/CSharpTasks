886560c39673ec609f3636c9a5b440c9abab011e
alekseichernyshev <adanayrel@mail.ru>
2022-12-03 18:26:28 +0900

    Задача с выявлением является ли первое число квадратом второго первая итерация

---
+++ Ex001 _Square2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1. По двум заданным числам проверить является ли первое число квадратом второго
+
+int numberA = new Random().Next(5, 25);
+int numberB = new Random().Next(1, 5);
+Console.WriteLine("Первое число получилось такое:");
+Console.WriteLine(numberA);
+Console.WriteLine("Второе число получилось такое:");
+Console.WriteLine(numberB);
+
+if (numberA - numberB * numberB == 0)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
