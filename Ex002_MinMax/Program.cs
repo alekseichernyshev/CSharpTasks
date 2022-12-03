10aca6a6204977dff1e636496f2d0d44c548901b
alekseichernyshev <adanayrel@mail.ru>
2022-12-03 19:36:58 +0900

    Третья задача плюс обновили readme

---
+++ Ex002_MinMax/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2 определить максимальное и минимальное число
+
+int numberA = new Random().Next(1, 1000);
+int numberB = new Random().Next(1, 1000);
+Console.WriteLine("Первое число");
+Console.WriteLine(numberA);
+Console.WriteLine("Второе число");
+Console.WriteLine(numberB);
+
+if (numberA > numberB)
+{
+    Console.WriteLine("Максимальное");
+    Console.WriteLine(numberA);
+    Console.WriteLine("Минимальное");
+    Console.WriteLine(numberB);
+}
+else
+{
+    Console.WriteLine("Максимальное");
+    Console.WriteLine(numberB);
+    Console.WriteLine("Минимальное");
+    Console.WriteLine(numberA);
+}
