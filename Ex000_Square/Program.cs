7d2fd6e553dca904b75684b7b4381cfa03b567d0
alekseichernyshev <adanayrel@mail.ru>
2022-12-04 18:12:48 +0900

    сделали ввод числа с клавиатуры

--- Ex000_Square/Program.cs
+++ Ex000_Square/Program.cs
@@ -1,7 +1,6 @@
 ﻿// Задача 0 Вывести квадрат числа. Вернуться после изучения ввода данных.
 
-int number = new Random().Next(2, 1000);
-Console.WriteLine("Вот такое число нарандомили:");
-Console.WriteLine(number);
-Console.WriteLine("Вычисляем...");
+Console.Write("Введите число и нажмите клавишу Enter\t");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Квадрат числа равен");
 Console.WriteLine(number * number);
---
+++ Ex003_Week/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 4. По заданному номеру дня недели вывести его название
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int monday = 1;
+int tuesday = 2;
+int wednesday = 3;
+int thursday = 4;
+int friday = 5;
+int saturday = 6;
+int sunday = 7;
